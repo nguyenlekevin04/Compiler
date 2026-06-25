6aa5073956ece7638583ccb882e713d150df991b
Kevin Le Nguyen <s88877@stud.htw-dresden.de>
2026-06-25 22:08:20 +0200

    feat: Add lexer and parser implementation for basic arithmetic expressions

--- src/parser.c
+++ src/parser.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../include/token.h"
 #include "../include/lexer.h"
@@ -8,49 +9,110 @@ Token getNextToken(Lexer* l) {
     return GetToken(l);
 }
 
-Ast* Parse(Lexer* l) {
-    Token tok = getNextToken(l);
+Ast* parseFactor(Lexer* l) {
+    Token t1 = getNextToken(l);
 
-    if(tok.Type != TOKEN_DIGIT) return NULL;
-
-    Ast* leaf = malloc(sizeof(Ast));
-    leaf->AstType = DIGIT;
-    leaf->value = atoi(tok.Lexeme);
-    leaf->left = leaf->right = NULL;
-    Ast* current = leaf;
-
-    while(1) {
-        Token op = getNextToken(l);
-
-        if(op.Type == TOKEN_EOF) break;
-
-        if(op.Type != TOKEN_PLUS && op.Type != TOKEN_DIV && op.Type != TOKEN_MINUS && op.Type != TOKEN_MULT) return NULL;
-
-        Ast* AstOp = malloc(sizeof(Ast));
-        AstOp->AstType = (op.Type == TOKEN_PLUS) ? PLUS :
-                         (op.Type == TOKEN_DIV) ? DIVIDE :
-                         (op.Type == TOKEN_MINUS) ? MINUS :
-                         (op.Type == TOKEN_MULT) ? MULT : INVALID;
-
-        AstOp->left = current;
-
-        Token dig = getNextToken(l);
-
-        if(dig.Type != TOKEN_DIGIT) return NULL;
-
-        Ast* AstRight = malloc(sizeof(Ast));
-        AstRight->value = atoi(dig.Lexeme);
-        AstRight->AstType = DIGIT;
-        AstRight->left = AstRight->right = NULL;
-        AstOp->right = AstRight;
-
-        current = AstOp;
+    if (t1.Type == TOKEN_DIGIT) {
+        Ast* node = (Ast*)malloc(sizeof(Ast));
+        node->AstType = DIGIT;
+        node->value = atoi(t1.Lexeme);
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+    } else {
+        printf("Error: Expected a digit, but got %s\n", t1.Lexeme);
+        return NULL;
     }
-    return current;
 }
 
-void FreeAst(Ast* ast) {
-    if(ast) {
-        free(ast);
+Ast* parseTerm(Lexer* l) {
+    Ast* left = parseFactor(l);
+
+    Token t1 = peekToken(l);
+    if (t1.Type == TOKEN_MULT) {
+        GetToken(l);
+        Ast* node = (Ast*)malloc(sizeof(Ast));
+        node->AstType = MULT;
+        node->left = left;
+        node->right = parseTerm(l);
+        return node;
+    } else if (t1.Type == TOKEN_DIV) {
+        GetToken(l);
+        Ast *node = (Ast*)malloc(sizeof(Ast));
+        node->AstType = DIVIDE;
+        node->left = left;
+        node->right = parseTerm(l);
+        return node;
+    } else {
+        return left;
     }
+}
+
+Ast* parseExpression(Lexer* l) {
+    Ast* left = parseTerm(l);
+
+    Token t1 = peekToken(l);
+    if (t1.Type == TOKEN_PLUS) {
+        GetToken(l);
+        Ast* node = (Ast*)malloc(sizeof(Ast));
+        node->AstType = PLUS;
+        node->left = left;
+        node->right = parseExpression(l);
+        return node;
+    } else if (t1.Type == TOKEN_MINUS) {
+        GetToken(l);
+        Ast* node = (Ast*)malloc(sizeof(Ast));
+        node->AstType = MINUS;
+        node->left = left;
+        node->right = parseExpression(l);
+        return node;
+    } else {
+        return left;
+    }
+}
+
+void printAst(Ast* node) {
+    if (node == NULL) {
+        return;
+    }
+
+    printAst(node->left);
+
+    switch (node->AstType) {
+        case DIGIT:
+            printf("%d ", node->value);
+            break;
+        case PLUS:
+            printf("+ ");
+            break;
+        case MINUS:
+            printf("- ");
+            break;
+        case MULT:
+            printf("* ");
+            break;
+        case DIVIDE:
+            printf("/ ");
+            break;
+        default:
+            printf("Invalid AST node type\n");
+    }
+
+    printAst(node->right);
+}
+
+int main() {
+    char input[256];
+    printf("Enter an expression: ");
+    fgets(input, sizeof(input), stdin);
+
+    Lexer* lexer = New(input);
+    Ast* ast = parseExpression(lexer);
+
+    printf("Parsed AST (in-order traversal): ");
+    printAst(ast);
+    printf("\n");
+
+    FreeLexer(lexer);
+    return 0;
 }
